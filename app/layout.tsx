659b0f8827d80a0afa567421a474be885ca410f2
yetzerBDev <yetzerBDev@users.noreply.github.com>
2026-08-11 08:38:31 -0600

    feat: pagina principal con proposito de la app y footer legal

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
